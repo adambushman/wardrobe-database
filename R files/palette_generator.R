c657c6483c0d8121af5ba7aec912a2cf07fad822
adambushman <adambushman@mail.weber.edu>
2022-11-28 12:13:32 -0700

    Fixed transformation pipeline to be more seemless

--- R files/palette_generator.R
+++ R files/palette_generator.R
@@ -8,6 +8,23 @@ library('lubridate')
 # Get Color Data
 ###
 
+execute <- function(query) {
+  mydb = dbConnect(MySQL()
+                   , user='AdamB'
+                   , password='clt@42jSKNdXKzoHsV3Y'
+                   , dbname='wardrobeDB'
+                   , host='localhost')
+  
+  res <- dbSendQuery(mydb, query)
+  
+  raw <- dbFetch(res, n=-1) %>% data.frame(.)
+  
+  dbClearResult(res)
+  dbDisconnect(mydb)
+  
+  return(raw)
+}
+
 get_colors <- function(type = c("items", "fits"), dateRange = NULL) {
   mydb = dbConnect(MySQL()
                    , user='AdamB'
@@ -47,29 +64,28 @@ get_colors <- function(type = c("items", "fits"), dateRange = NULL) {
     summarise(totalShare = sum(totalShare), .groups = 'drop')
 }
 
-
-colors.all <- 
-  get_colors("items") %>%
+# Transformation Pipeline
+execute("SELECT * FROM vAllItemColors") %>%
+  group_by(hexCode, commonName) %>%
+  summarise(totalShare = sum(totalShare), .groups = 'drop') %>%
   mutate(
     type = "What I Own"
   ) %>%
   select(type, hexCode, commonName, totalShare) %>%
   rbind(
-    get_colors("fits") %>%
+    execute(
+      "SELECT * FROM vAllFitColors
+      WHERE fitDate BETWEEN DATE(NOW() - INTERVAL 3 MONTH) AND NOW()"
+    ) %>%
+      group_by(hexCode, commonName) %>%
+      summarise(totalShare = sum(totalShare), .groups = 'drop') %>%
       mutate(
         type = "What I Wear"
       ) %>%
       select(type, hexCode, commonName, totalShare)
-  )
-
-
-###
-# Print Palette Function
-###
-
-
-p.data <- 
-  colors.all %>%
+  ) %>%
+  assign("colors_copy", ., envir = .GlobalEnv) %>%
+  # Next step
   group_by(type, commonName, hexCode) %>%
   summarise(share = sum(totalShare), .groups = 'drop') %>%
   group_by(type, commonName) %>%
@@ -79,7 +95,7 @@ p.data <-
     ymin = ymax - share
   ) %>%
   inner_join(
-    colors.all %>%
+    colors_copy %>%
       group_by(type, commonName) %>%
       summarise(share = sum(totalShare), .groups = 'drop') %>%
       group_by(type) %>%
@@ -100,11 +116,11 @@ p.data <-
     ymax_s = ymax / max(ymax, ymin), 
     ymin_s = ymin / max(ymax, ymin)
   ) %>%
-  select(type.x, commonName.x, hexCode, xmax_s, xmin_s, ymax_s, ymin_s)
-
- 
-
-ggplot(p.data, aes(xmin = xmin_s, xmax = xmax_s, ymin = ymin_s, ymax = ymax_s, 
+  select(type.x, commonName.x, hexCode, xmax_s, xmin_s, ymax_s, ymin_s) %>%
+  assign("p.data", ., envir = .GlobalEnv) %>%
+  
+  # Beginning the plot
+  ggplot(., aes(xmin = xmin_s, xmax = xmax_s, ymin = ymin_s, ymax = ymax_s, 
              fill = hexCode)) +
   geom_rect(show.legend = FALSE, fill = p.data$hexCode) +
   facet_wrap(~type.x, nrow = 2, ncol = 1) + 
@@ -115,9 +131,9 @@ ggplot(p.data, aes(xmin = xmin_s, xmax = xmax_s, ymin = ymin_s, ymax = ymax_s,
   theme(
     plot.title = element_text(hjust = 0.5, face = "bold", color = "#53c3ce"), 
     plot.subtitle = element_text(hjust = 0.5, face = "italic", color = "#333333"), 
-    plot.background = element_rect(fill = "#f1f7f4"), 
+    plot.background = element_rect(fill = "#D7DEDC"), 
     
-    panel.background = element_rect(fill = "#f1f7f4"), 
+    panel.background = element_rect(fill = "#D7DEDC"), 
     panel.grid = element_blank(), 
     
     strip.background = element_rect(color = "#333333", fill = NA), 
