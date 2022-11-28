323ed0bd26b95baaabeba4a587c5b2d3ca9c8c05
adambushman <adambushman@mail.weber.edu>
2022-11-28 15:23:06 -0700

    Adjusted function for executing MySQL query

--- Python files/sql-scripts-in-python.py
+++ Python files/sql-scripts-in-python.py
@@ -1,53 +1,30 @@
-import mysql.connector
-from mysql.connector import Error
 import pandas as pd
+import mysql.connector
+
+# Function to execute a query and return a Pandas Data Frame
+def execute(query):
+    # Establish a connection
+    cnx = mysql.connector.connect(
+        host = 'localhost',
+        user = 'AdamB',
+        passwd = 'clt@42jSKNdXKzoHsV3Y',
+        database = 'wardrobedb'
+    )
+
+    # Execute
+    cursor = cnx.cursor()
+    cursor.execute(query)
+
+    # Create Pandas Data Frame with correct column names
+    real_cols = dict(map(lambda i,j : (i,j) , list(range(0, len(cursor.column_names))), cursor.column_names))
+    data = pd.DataFrame(cursor).rename(real_cols, axis = 1)
+
+    # Close connection
+    cursor.close()
+    cnx.close()
+
+    return(data)
 
 
-# Function to connect to database
-def create_db_connection(host_name, user_name, user_password, db_name):
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host =host_name,
-            user = user_name,
-            passwd = user_password,
-            database = db_name
-        )
-        print("Connection successful")
-    except Error as err:
-        print(f"Error: '{err}'")
 
-    return connection
-
-# Function to execute a non-display queries
-def execute_query(connection, query):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        connection.commit()
-        print("Query successful")
-    except Error as err:
-        print(f"Error: '{err}'")
-
-# Function to execute a query and return the display
-def read_query(connection, query):
-    cursor = connection.cursor()
-    result = None
-    try:
-        cursor.execute(query)
-        result = cursor.fetchall()
-        columns = cursor
-        return zip(columns, result)
-    except Error as err:
-        print(f"Error: '{err}'")
-
-
-# Connection to WardrobeDB
-wd_connection = create_db_connection("localhost", "AdamB", "clt@42jSKNdXKzoHsV3Y", "wardrobedb")
-
-# Test query
-tq = "SELECT * FROM wItem;"
-
-test = read_query(wd_connection, tq)
-
-itemTable = pd.DataFrame(test, columns = ["itemID"])
+test = execute("SELECT * FROM vAllFitColors;")
